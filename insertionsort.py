dd758be6e8e15155c8d3731ea4324e2f70583310
embydextrous <arjitagarwal099@gmail.com>
2020-09-08 14:48:40 +0530

    Insertion sort in descending order

--- insertionsort.py
+++ insertionsort.py
@@ -1,4 +1,4 @@
-def sort(a):
+def sortAsc(a):
     n = len(a)
     for j in range(1, n):
         key = a[j] # Cache Key
@@ -7,8 +7,18 @@ def sort(a):
             a[i+1] = a[i]
             i -= 1
         a[i+1] = key # Insert current key into found position
+
+def sortDesc(a):
+    n = len(a)
+    for j in range(1, n):
+        key = a[j]
+        i = j - 1
+        while i>=0 and a[i] < key:
+            a[i+1] = a[i]
+            i -= 1
+        a[i+1] = key
     
 a = [5, 2, 4, 6, 1, 3]
 print a
-sort(a)
+sortDesc(a)
 print a
