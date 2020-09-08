0a98adde3390b11f158529e749615f4b63346df8
embydextrous <arjitagarwal099@gmail.com>
2020-09-08 15:30:57 +0530

    Selection Sort Implementation

---
+++ selectionSort.py
@@ -0,0 +1,13 @@
+# Select minimum element
+def sort(a):
+    n = len(a)
+    for i in range(n-1):
+        minIndex = i
+        for j in range(i, n):
+            if a[j] < a[minIndex]:
+                minIndex = j
+        a[minIndex], a[i] = a[i], a[minIndex]
+
+a = [5, 2, 7, 4, 6, 1, 3]
+sort(a)
+print a
