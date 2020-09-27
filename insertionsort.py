45f7998bbc74d1b60b81a5ad4559cabd2c591819
embydextrous <arjitagarwal099@gmail.com>
2020-09-28 00:42:23 +0530

    Insertion sort recursive

--- insertionsort.py
+++ insertionsort.py
@@ -16,12 +16,25 @@ def sortDesc(a):
     for j in range(1, n):
         key = a[j]
         i = j - 1
-        while i>=0 and a[i] < key:
+        while i >=0 and a[i] < key:
             a[i+1] = a[i]
             i -= 1
         a[i+1] = key
+
+def insertionSortRecursive(a, n, i):
+    if i >= n:
+        return
+    key = a[i]
+    j = i - 1
+    while j >= 0 and a[j] > key:
+        a[j+1] = a[j]
+        j -= 1
+    a[j+1] = key
+    insertionSortRecursive(a, n, i + 1)
+
+
     
 a = [5, 2, 4, 6, 1, 3]
 print a
-sortDesc(a)
+insertionSortRecursive(a, len(a), 1)
 print a
