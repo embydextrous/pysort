15bd7e8bc7ef6604ec1b4fa6c1023fae2d4cab80
embydextrous <arjitagarwal099@gmail.com>
2020-10-03 00:55:39 +0530

    Mergesort Iterative

--- mergeSort.py
+++ mergeSort.py
@@ -45,8 +45,50 @@ def mergeSort2(a):
         a += R
     return a
 
+def mergeSortIterative(a):
+    currentSize = 1
+    while currentSize < len(a) - 1:
+        left = 0
+        while left < len(a) - 1:
+            mid = min(left + currentSize - 1, len(a) - 1)
+            right = min(left + 2 * currentSize - 1, len(a) - 1)
+            merge(a, left, mid, right)
+            left += 2 * currentSize
+        currentSize *= 2
+
+def merge(a, l, m, r):
+    n1 = m - l + 1
+    n2 = r - m
+    L = [-1] * n1
+    R = [-1] * n2
+    for i in range(n1):
+        L[i] = a[l+i]
+    for i in range(n2):
+        R[i] = a[m+1+i]
+    i = j = 0
+    k = l
+    while i < n1 and j < n2:
+        if L[i] <= R[j]:
+            a[k] = L[i]
+            i += 1
+        else:
+            a[k] = R[j]
+            j += 1
+        k += 1
+    
+    while i < n1:
+        a[k] = L[i]
+        k += 1
+        i += 1
+    
+    while j < n2:
+        a[k] = R[j]
+        k += 1
+        j += 1
+
+
 a = [12, 11, 13, 5, 6, 7]
-mergeSort(a)
+mergeSortIterative(a)
 print a
 
 
